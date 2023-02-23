fa8dea70d132bb607535350e82c635697ef97697
danikaaa <dltndus9563@naver.com>
2023-02-24 03:26:19 +0900

    commit

--- src/js/info.js
+++ src/js/info.js
@@ -1,11 +1,17 @@
 
 
 function Info() {
+
+    function handleClick(e){
+        e.preventDefault();
+        console.log('클릭');
+    }
+
     return (
       <div className="info">
         깔쌈하게 나눠볼까요?
         <div className="people-box">
-            <span className="plus-btn">함께한 사람 추가 +</span>
+            <span className="plus-btn" onClick={handleClick}>함께한 사람 추가 +</span>
         </div>
         <div className="input-box">
             <input type="text" placeholder="함께한 사람의 이름을 적어주세요." id="people"></input>
