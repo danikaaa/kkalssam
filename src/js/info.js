f646e142638c46a3dd4fbc9b95f6f8c541dda43e
danikaaa <dltndus9563@naver.com>
2023-02-24 03:54:54 +0900

    event

--- src/js/info.js
+++ src/js/info.js
@@ -5,6 +5,8 @@ function Info() {
     function handleClick(e){
         e.preventDefault();
         console.log('클릭');
+        document.getElementById('name').classList.remove('none');
+        // 클래스 명에 따라 css처리
     }
 
     return (
@@ -13,20 +15,20 @@ function Info() {
         <div className="people-box">
             <span className="plus-btn" onClick={handleClick}>함께한 사람 추가 +</span>
         </div>
-        <div className="input-box">
-            <input type="text" placeholder="함께한 사람의 이름을 적어주세요." id="people"></input>
+        <div className="input-box none" id="name">
+            <input type="text" placeholder="함께한 사람의 이름을 적어주세요." id="people"></input><span>+</span>
         </div>
         <div className="food-box">
-            <span className="plus-btn">내용 추가 +</span>
+            <span className="plus-btn" onClick={handleClick}>내용 추가 +</span>
         </div>
         <div className="food">
-            <div className="input-box">
+            <div className="input-box none">
             <input type="text" placeholder="내용을 입력해주세요." id="food-content"></input>
             </div>
-            <div className="input-box">
+            <div className="input-box none">
             <input type="text" placeholder="금액을 입력해주세요." id="food-price"></input>원
             </div>
-            <div>여긴 for문 돌려서 체크박스스</div>
+            <div className="input-box none">여긴 for문 돌려서 체크박스스</div>
         </div>
         
       </div>
