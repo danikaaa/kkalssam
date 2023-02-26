a3d54dfbe8be69d8a25b5819bac2e6b3430d6607
danikaaa <dltndus9563@naver.com>
2023-02-27 00:17:56 +0900

    people event add

--- src/js/info.js
+++ src/js/info.js
@@ -2,11 +2,24 @@
 
 function Info() {
 
+    const peopleArr = [];
+
     function handleClick(e){
         e.preventDefault();
         console.log('클릭');
         document.getElementById('name').classList.remove('none');
         // 클래스 명에 따라 css처리
+    }
+
+    function peoplePlus(e){
+        e.preventDefault();
+
+        const name = document.getElementById("people").value;
+
+        peopleArr.push(name);
+
+        console.log(document.getElementById("people").value);
+        console.log(peopleArr);
     }
 
     return (
@@ -16,7 +29,14 @@ function Info() {
             <span className="plus-btn" onClick={handleClick}>함께한 사람 추가 +</span>
         </div>
         <div className="input-box none" id="name">
-            <input type="text" placeholder="함께한 사람의 이름을 적어주세요." id="people"></input><span>+</span>
+            <input type="text" placeholder="함께한 사람의 이름을 적어주세요." id="people"></input><span onClick={peoplePlus}>+</span>
+        </div>
+        <div>
+            <span>함께한 사람</span>
+            <ul>
+                <li>사람1</li>
+                <li>사람2</li>
+            </ul>
         </div>
         <div className="food-box">
             <span className="plus-btn" onClick={handleClick}>내용 추가 +</span>
