7cb52f6b4eb8ccf7d59ce6eb1633b9349cbde1fe
danikaaa <dltndus9563@naver.com>
2023-10-17 22:47:44 +0900

    배포

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import {Routes, Route} from 'react-router-dom';
+import {HashRouter as Routes, Route} from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Receipt from './pages/Receipt';
 import Loding from './pages/Loding';
+import Logo from './pages/Logo';
 
 function App() {
   return (
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/kkalssam" element={<Home />} />
         <Route path="/Receipt" element={<Receipt />} />
         <Route path="/Loding" element={<Loding />} />
+        <Route path="/Logo" element={<Logo />} />
       </Routes>
 
    
